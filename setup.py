d32e4e1da3f1d882a49e015a55b3a7855a2c2bac
ttruong1000 <90523390+ttruong1000@users.noreply.github.com>
2023-11-22 10:46:39 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import socket
+
+# TCP Sockets
+class MPM210HInterface:
+    def __init__(ipsrc, psrc, ipdst, pdst):
+        socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+    def __write_command__(): #Private function
+      
+    def __query_command__(): #Private function
+      return byte_string
+   
+    #Public functions
+    def meas():
+      
+    def zero():
+      
+    def loggnn():
+      
+    def __del__():
+        socket.close()
